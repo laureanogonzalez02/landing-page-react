342a24e2ce9a7faa830619ea2a5e4af15e7bdbdd
Laureano <laugx2002@hotmail.com>
2025-12-12 17:45:26 -0300

    finished card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,11 +2,12 @@ import "./Card.css";
 
 function Card() {
   return (
-    <section className="card">
+    <section className="card rounded-5">
       <div className="card-body">
-        <h3 className="card-title">Advanced Techniques</h3>
+        <div className="card-number mt-2">01.</div>
+        <h3 className="card-title mb-3">Advanced Techniques</h3>
         <p className="card-text">
-          state-of-the-art methods such as hydraulic fracturing, horizontal
+          State-of-the-art methods such as hydraulic fracturing, horizontal
           drilling, and enhanced oil recovery (EOR) to maximize extraction
           efficiency while minimizing environmental impact.
         </p>
